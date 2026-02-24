ed8e9d809644329e9ab0a32a94b4cbf31bdd1277
CrisPonte <crisponte@github.com>
2026-02-24 16:12:08 -0300

    Version 0.5 Anda sin localhost hardcodeado

--- api/test_login.js
+++ api/test_login.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function test() {
     try {
-        const res = await axios.post('http://localhost:5000/api/auth/login', {
+        const res = await axios.post('/api/auth/login', {
             email: 'admin@hr-talent.local',
             password: '123456'
         });
--- api/test_login2.js
+++ api/test_login2.js
@@ -1,6 +1,6 @@
 async function test() {
     try {
-        const res = await fetch('http://localhost:5000/api/auth/login', {
+        const res = await fetch('/api/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email: 'admin@hr-talent.local', password: '123456' })
